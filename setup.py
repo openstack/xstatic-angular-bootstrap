5fc34187e1c48deee5c3727de4619960c6863369
Richard Jones <r1chardj0n3s@gmail.com>
2016-06-24 13:46:02 +1000

    Include more metadata in setup.py
    This uses the newer 1.1.0 version of xstatic-release
    which copies more information into setup.py and therefore
    into pypi.
    
    This change will also be used to test releasing through
    the openstack-release system.
    
    Change-Id: I036c1ce0b74212e74dffa853e709db086a4829be

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
+
+# The README.txt file should be written in reST so that PyPI can use
+# it to generate your project's PyPI page.
+long_description = open('README.txt').read()
+
 setup(
     name='XStatic-Angular-Bootstrap',
+    summary="""Angular-Bootstrap 0.11.0 (XStatic packaging standard)""",
+    description=long_description,
+    maintainer="Maxime Vidori",
+    maintainer_email='maxime.vidori@enovance.com',
     use_scm_version=True,
     setup_requires=['setuptools_scm', 'wheel'],
     packages=find_packages()
